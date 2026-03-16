7980133774bb2a793a760522c7eb5b4384165cbb
Gwiin <injoa4526@gmail.com>
2026-03-16 09:36:44 +0900

    0316

--- c_example/part4/baseballGame/baseball.h
+++ c_example/part4/baseballGame/baseball.h
@@ -1,3 +1,6 @@
+#ifndef BASEBALL_H
+#define BASEBALL_H
 void generate_number(int* question);
 void input_number(int* answer);
 bool check_result(const int* question, const int* answer, int* strike, int* ball);
+#endif
