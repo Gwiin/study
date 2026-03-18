822017fc83157be7bed68f569676878a85ea4ab1
Gwiin <injoa4526@gmail.com>
2026-03-18 13:31:54 +0900

    myqueue

---
+++ ds/queue/myQueue1/queue.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdio.h>
+#define QUEUESIZE 100
+
+typedef struct {
+    int queue[QUEUESIZE];
+    int *front;
+    int *rear;
+}Queue;
+
+
+void initQueue(Queue *pq);
+void push(Queue *pq, int data);
+int pop(Queue *pq);
