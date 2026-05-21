1e2ccc7e482ebdb606b5b45d30c54da8d929657c
gwiin <injoa4526@gmail.com>
2026-05-21 11:01:43 +0900

    interpreter

--- python_example/basic/hello.py
+++ python_example/basic/hello.py
@@ -1,1 +1,10 @@
-print("hello, world")
+def main():
+    print("hello, world")
+
+
+
+# import 를 당했을 때 __main__ 이 아니게 된다.
+# import 를 당하면 파일명이 name이 된다.
+if __name__ == "__main__": # __name__ 숨은 변수
+    main()
+
