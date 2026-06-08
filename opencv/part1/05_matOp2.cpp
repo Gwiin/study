df01f2b42d2ead2d44456dd85643f20220f0202b
gwiin <injoa4526@gmail.com>
2026-06-08 17:31:41 +0900

    Add OpenCV GStreamer examples

--- opencv/part1/05_matOp2.cpp
+++ opencv/part1/05_matOp2.cpp
@@ -20,6 +20,17 @@ int main()
     Mat img5;
     img1.copyTo(img5);
 
+    for (int i = 0; i < 10 ; i++){
+        for (int j = 0 ; j < 10 ; j++){
+            Vec3b &pixel =  img5.at<Vec3b>(i,j);
+            cout << pixel[0] << pixel << pixel[2] << endl;
+            pixel[0] = 0;
+            pixel[1] = 0;
+            pixel[2] = 0;
+        }
+    }
+    
+
     img1.setTo(Color::Yellow);
 
     Rect roi(220, 120, 200, 200);
